e6305c6c0bf0a0201a2930638b9204393b44f0c0
dblock <dblock@amazon.com>
2022-12-26 14:29:47 -0500

    OpenSearch Go client demo with Sigv4 signing.
    Signed-off-by: dblock <dblock@amazon.com>

---
+++ README.md
@@ -0,0 +1,46 @@
+# OpenSearch Go Client Demo
+
+Makes requests to Amazon OpenSearch using the [OpenSearch Go Client](https://github.com/opensearch-project/opensearch-go).
+
+## Prerequisites
+
+### Go
+
+Install [Go](https://go.dev/doc/install). YMMV.
+
+```
+$ go version
+go version go1.19.4 darwin/arm64
+```
+
+## Running
+
+Create an OpenSearch domain in (AWS) which support IAM based AuthN/AuthZ.
+
+```
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_SESSION_TOKEN=
+export AWS_REGION=us-west2
+
+export OPENSEARCH_ENDPOINT=https://....us-west-2.es.amazonaws.com
+
+$ go run main.go
+```
+
+This will output the version of OpenSearch and a search result.
+
+```
+opensearch: 2.3.0
+[map[_id:1 _index:test-index _score:0.2876821 _source:map[director:Bennett Miller title:Moneyball year:2011]]]
+```
+
+The [code](main.go) will create an index, add a document to it, search, then cleanup.
+
+## License 
+
+This project is licensed under the [Apache v2.0 License](LICENSE.txt).
+
+## Copyright
+
+Copyright OpenSearch Contributors. See [NOTICE](NOTICE.txt) for details.
---
+++ main.go
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/opensearch-project/opensearch-go/v2"
+	requestsigner "github.com/opensearch-project/opensearch-go/v2/signer/awsv2"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg, _ := config.LoadDefaultConfig(ctx)
+	signer, _ := requestsigner.NewSigner(cfg)
+
+	endpoint, present := os.LookupEnv("OPENSEARCH_ENDPOINT")
+	if !present {
+		log.Fatal("OPENSEARCH_ENDPOINT missing")
+	}
+
+	client, _ := opensearch.NewClient(opensearch.Config{
+		Addresses: []string{endpoint},
+		Signer:    signer,
+	})
+
+	if info, err := client.Info(); err != nil {
+		log.Fatal("info", err)
+	} else {
+		var r map[string]interface{}
+		json.NewDecoder(info.Body).Decode(&r)
+		version := r["version"].(map[string]interface{})
+		fmt.Printf("%s: %s\n", version["distribution"], version["number"])
+	}
+
+	// create an index
+	if _, err := client.Indices.Create("test-index", client.Indices.Create.WithWaitForActiveShards("1")); err != nil {
+		log.Fatal("indices.create", err)
+	}
+
+	// index a document
+	document, _ := json.Marshal(map[string]interface{}{
+		"title":    "Moneyball",
+		"director": "Bennett Miller",
+		"year":     "2011",
+	})
+
+	if _, err := client.Index("test-index", strings.NewReader(string(document)), client.Index.WithDocumentID(("1"))); err != nil {
+		log.Fatal("index", err)
+	}
+
+	// wait for the document to index
+	time.Sleep(1 * time.Second)
+
+	// search for the document
+	query, _ := json.Marshal(map[string]interface{}{
+		"query": map[string]interface{}{
+			"multi_match": map[string]interface{}{
+				"query":  "miller",
+				"fields": []string{"title^2", "director"},
+			},
+		},
+	})
+
+	if resp, err := client.Search(client.Search.WithBody(strings.NewReader(string(query)))); err != nil {
+		log.Fatal("index", err)
+	} else {
+		var r map[string]interface{}
+		json.NewDecoder(resp.Body).Decode(&r)
+		hits := r["hits"].(map[string]interface{})["hits"]
+		fmt.Println(hits)
+	}
+
+	// delete the document
+	if _, err := client.Delete("test-index", "1"); err != nil {
+		log.Fatal("delete", err)
+	}
+
+	// delete the index
+	if _, err := client.Indices.Delete([]string{"test-index"}, client.Indices.Delete.WithIgnoreUnavailable(true)); err != nil {
+		log.Fatal("indices.delete", err)
+	}
+}
